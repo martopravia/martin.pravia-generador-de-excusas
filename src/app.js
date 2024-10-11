3afb7c8b014c4ca04abf5d7e8a852ba0d342efc7
martopravia <marto.pravia@gmail.com>
2024-10-11 20:51:02 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuseGenerator() {
+  const randomNumberIndex = arr => Math.floor(Math.random() * arr.length);
+  let excuse =
+    who[randomNumberIndex(who)] +
+    " " +
+    action[randomNumberIndex(action)] +
+    " " +
+    what[randomNumberIndex(what)] +
+    " " +
+    when[randomNumberIndex(when)];
+  document.getElementById("excuse").innerHTML = excuse;
+}
+window.onload = excuseGenerator;
